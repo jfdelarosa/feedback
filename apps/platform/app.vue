0ea18f76d403f9ea7e5427cfd9d17bd9f41f8532
Fernando de la Rosa <hola@jfdelarosa.dev>
2025-05-16 20:49:12 -0600

    Init

---
+++ apps/platform/app.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// Components are automatically imported
+</script>
+
+<template>
+  <header>
+    <SignedOut>
+      <SignInButton />
+    </SignedOut>
+    <SignedIn>
+      <UserButton />
+    </SignedIn>
+  </header>
+
+  <main>
+    <NuxtPage />
+  </main>
+</template>
